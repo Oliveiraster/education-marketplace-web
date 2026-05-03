610f5d4107f9ba05684fac8903563b8180aa6ad2
Raphael Oliveira <oliveiraster.dev@gmail.com>
2026-05-02 23:21:11 -0300

    feat(footer): add footer component

---
+++ src/app/shared/components/footer/footer.spec.ts
@@ -0,0 +1,23 @@
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { Footer } from './footer';
+
+describe('Footer', () => {
+  let component: Footer;
+  let fixture: ComponentFixture<Footer>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Footer],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Footer);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
