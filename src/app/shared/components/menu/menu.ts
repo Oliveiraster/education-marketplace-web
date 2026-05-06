b2aa3cc77f7a2f6d72925734136c35532f710b2d
Raphael Oliveira <oliveiraster.dev@gmail.com>
2026-05-06 12:19:07 -0300

    fix(menu): add active state styling using routerLinkActive

--- src/app/shared/components/menu/menu.ts
+++ src/app/shared/components/menu/menu.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './menu.html',
   styleUrl: './menu.scss',
 })
